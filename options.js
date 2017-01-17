be699c2b0d518d9821df4aa5d4f5dc2a5ba04341
Oğuz Kağan EREN <oguzkaganeren@gmail.com>
2017-01-17 16:28:35 +0300

    tabloya drag drop özelliği eklendi

--- options.js
+++ options.js
@@ -6,15 +6,22 @@ var xhr = new XMLHttpRequest();
           var table=document.getElementById("mytable");
          for(var i=12;i>0 ;i--){
           var row= table.insertRow(1);
-          row.insertCell(0).innerHTML="<img src='icons/"+xmlDoc.getElementsByTagName("Currency")[(i-1)].getAttribute("Kod")+".png"+"' style='height:14px;width:13px'>"+xmlDoc.getElementsByTagName("Currency")[(i-1)].getAttribute("Kod")+"/TRY";
+          row.className='simple_with_animation';
+          row.insertCell(0).innerHTML="<img src='icons/"+xmlDoc.getElementsByTagName("Currency")[(i-1)].getAttribute("Kod")+".png"+"'>"+xmlDoc.getElementsByTagName("Currency")[(i-1)].getAttribute("Kod")+"/TRY";
           row.insertCell(1).innerHTML=xmlDoc.getElementsByTagName("Unit")[(i-1)].childNodes[0].nodeValue;
-          row.insertCell(2).innerHTML=xmlDoc.getElementsByTagName("ForexBuying")[(i-1)].childNodes[0].nodeValue;
-          row.insertCell(3).innerHTML=xmlDoc.getElementsByTagName("ForexSelling")[(i-1)].childNodes[0].nodeValue;
+          row.insertCell(2).innerHTML="<div class='radio'><input type='radio' name='optradio'>"+xmlDoc.getElementsByTagName("ForexBuying")[(i-1)].childNodes[0].nodeValue+"</label></div>";
+          row.insertCell(3).innerHTML="<div class='radio'><input type='radio' name='optradio'>"+xmlDoc.getElementsByTagName("ForexSelling")[(i-1)].childNodes[0].nodeValue+"</label></div>";
           row.insertCell(4).innerHTML=xmlDoc.getElementsByTagName("BanknoteBuying")[(i-1)].childNodes[0].nodeValue;
           row.insertCell(5).innerHTML=xmlDoc.getElementsByTagName("BanknoteSelling")[(i-1)].childNodes[0].nodeValue;
           //"<td>"+xmlDoc.getElementsByTagName("Isim")[i].childNodes[0].nodeValue+"</td>";
 
           }
+          $('.sorted_table').sortable({
+            containerSelector: 'table',
+            itemPath: '> tbody',
+            itemSelector: 'tr',
+            placeholder: '<tr class="placeholder"/>'
+          });
         }
     };
     var xmlDoc;
